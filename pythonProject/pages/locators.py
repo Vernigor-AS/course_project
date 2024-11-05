f804f6ccc4d12a07c9f841a0103b279468252ce8
Vernigor-AS <veanserg@gmail.com>
2024-11-05 16:41:27 +0300

    Добавлен класс ProductPageLocators для локаторов на странице продукта

--- pythonProject/pages/locators.py
+++ pythonProject/pages/locators.py
@@ -4,6 +4,13 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
+class ProductPageLocators():
+    ADD_PRODUCT_TO_CART = (By.CSS_SELECTOR, '.btn.btn-lg.btn-primary')
+    NAME_OF_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6 h1')
+    PRODUCT_ADD_TO_CARD_CHECK = (By.CSS_SELECTOR, '#messages > div:nth-child(1) > div > strong')
+    MESSAGE_PRICE_ADDED_PRODUCT_IN_CART = (By.CSS_SELECTOR, '.alertinner p strong')
+    PRICE_OF_PRODUCT = (By.CSS_SELECTOR, '.col-sm-6 p.price_color')
+
 class LoginPageLocators():
     #Зарегистрированный пользователь
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
@@ -17,6 +24,8 @@ class LoginPageLocators():
     REGISTRATION_NEW_USER_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
     REGISTRATION_NEW_USER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password2')
     REGISTRATION_SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button[value="Register"]')
+
+
 
 
 
